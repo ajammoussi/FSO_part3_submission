b1f0a4ef9512695504039d2ba89569deef38b97a
ajammoussi <jam.ahmed55@gmail.com>
2024-07-20 20:44:06 +0100

    uibuild

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan")
 const app = express()
 
 app.use(express.json())
+app.use(express.static("dist"))
 
 let persons = [
     {
@@ -102,7 +103,7 @@ app.post("/api/persons", (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
