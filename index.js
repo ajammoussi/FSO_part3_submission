e6c0904d968f626125756952994503db9cabd156
ajammoussi <jam.ahmed55@gmail.com>
2024-07-21 19:48:18 +0100

    exercises 3.17

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.delete("/api/persons/:id", (request, response) => {
         .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const body = request.body
 
     const name = body.name
@@ -85,10 +85,27 @@ app.post("/api/persons", (request, response) => {
         number,
     });
 
-    person.save().then((savedPerson) => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then((savedPerson) => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 
+})
+
+app.put("/api/persons/:id", (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
