a0ce5cac055016d3155df3d09251c36f7cb3863b
ajammoussi <jam.ahmed55@gmail.com>
2024-07-21 19:18:40 +0100

    exercises 3.14 3.15

--- index.js
+++ index.js
@@ -48,40 +48,38 @@ app.get("/info", (request, response) => {
 })
 
 app.delete("/api/persons/:id", (request, response) => {
-    const id = request.params.id
-    persons = persons.filter((person) => person.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post("/api/persons", (request, response) => {
     const body = request.body
 
-    if (!body.name) {
+    const name = body.name
+    const number = body.number
+
+    if (!name) {
         return response.status(400).json({
             error: "name of person missing"
         })
-    } else if (!body.number) {
+    } else if (!number) {
         return response.status(400).json({
             error: "number of person missing"
         })
-    } else if (persons.find((person) => person.name === body.name)) {
-        return response.status(400).json({
-            error: "name must be unique"
-        })
     }
 
-    const id = Math.floor(Math.random() * 100000)
+    const person = new Person({
+        name,
+        number,
+    });
 
-    const person = {
-        id: String(id),
-        name: body.name,
-        number: body.number
-    }
+    person.save().then((savedPerson) => {
+        response.json(savedPerson)
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
 })
 
 const PORT = process.env.PORT
