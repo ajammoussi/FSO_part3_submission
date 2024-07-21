5ef4578a27f788a57b8a0f6cd882cfcd22b11077
ajammoussi <jam.ahmed55@gmail.com>
2024-07-21 17:59:52 +0100

    exercise 2.13

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
+require("dotenv").config()
 const express = require("express")
+const app = express();
 const morgan = require("morgan")
-const app = express()
+const cors = require("cors")
+const Person = require("./models/person")
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static("dist"))
 
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
-];
 
 app.use(
     morgan((tokens, req, res) => {
@@ -39,31 +21,30 @@ app.use(
         tokens["response-time"](req, res),
         "ms",
         JSON.stringify(req.body),
-    ].join(" ");
+    ].join(" ")
     })
 );
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
+    Person.find({}).then((persons) => {
+        response.json(persons)
+    })
 })
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = request.params.id;
-    const person = persons.find((person) => person.id === id)
-
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+    Person.findById(request.params.id).then((person) => {
+        response.json(person)
+    })
 })
 
 app.get("/info", (request, response) => {
-    const date = new Date();
-    response.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
-        <p>${date}</p>`
-    );
+    Person.find({}).then((persons) => {
+        const date = new Date()
+        response.send(
+            `<p>Phonebook has info for ${persons.length} people</p>
+            <p>${date}</p>`
+        )
+    })
 })
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -103,7 +84,7 @@ app.post("/api/persons", (request, response) => {
     response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
